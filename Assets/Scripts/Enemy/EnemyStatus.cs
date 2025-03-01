e87db7afabe0e9228061e7d0c00099c9ce5f3506
Vitor Pio <vitormarquespio@gmail.com>
2025-03-01 09:15:54 -0300

    add death animation

--- Assets/Scripts/Enemy/EnemyStatus.cs
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class EnemyStatus : MonoBehaviour
 {
     Rigidbody rb;
+    Animator animator;
 
     int health = 3;
     public bool isTakingDamage = false;
     readonly float timeBetweenDamage = 0.5f;
+    readonly float deathAnimationTime = 0.3f;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     public void TakeDamage(Vector3 playerPosition, int damage, float attackForce)
@@ -19,13 +22,19 @@ public class EnemyStatus : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            animator.SetTrigger("Die");
+            Invoke(nameof(DestroyEnemy), deathAnimationTime);
         }
         else
         {
             ApplyDamageForce(playerPosition, attackForce);
             Invoke(nameof(RecoverFromDamage), timeBetweenDamage);
         }
+    }
+
+    void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 
     void ApplyDamageForce(Vector3 playerPosition, float attackForce)
